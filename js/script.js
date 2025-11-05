fd3ad39ba4f9a2f949c099491fabfee4ddabc378
Niakate <nktmakan7@gmail.com>
2025-11-05 16:20:55 +0100

    finsh

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
-let ajouter = document.getElementById("ajouter")
+let ajoute = document.getElementById("ajouter");
+let selection = document.getElementById("selected1");
+let selection2 = document.getElementById("selected2");
+let commentaire = document.getElementById("comment");
+let gris1 = document.getElementById("gris1")
+let parent = document.getElementById("parent")
+let error = document.getElementById("error")
+
+
+ajoute.addEventListener("click", function () {
+
+let urgent = "";
+    if (commentaire.value == "") {
+        error.innerText = "Ajoute un commentaire";
+    } else {
+        error.innerText = "";
+           if (urgent = "Urgent") {
+            urgent = `<i class=" m-2 text-danger fa-solid fa-triangle-exclamation"></i>`
+        }
+        parent.innerHTML +=`
+        
+        <div class="postit1 p-2 m-3 mx-auto">
+            <div class="text-end">
+                <p class="croix mb-0"><i class="bi bi-x-octagon"></i></p>
+            </div>
+              <p class="h4 text-center">${urgent}${selection.value}</p>
+            <p class="h4 text-center">${selection2.value}</p>
+            <p class="h">${commentaire.value}</p>
+            </div>
+        </div>
+`
+}
+
+  commentaire.value = "";
 
 
 
-ajouter.addEventListener("click" , function (){
 
-})
+
+});
+
+
